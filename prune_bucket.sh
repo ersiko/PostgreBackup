2b1c33e12f3c4e51ffcd71bdac45cb5683b6179b
tomas@criptos.com <tomas@delightful.(none)>
2013-02-16 04:12:47 +0100

    Extracting vars on prune_bucket

--- prune_bucket.sh
+++ prune_bucket.sh
@@ -2,10 +2,13 @@
 
 . $(dirname $0)/backup_vars
 
+daily_backups=7
+weekly_backups=53
+
 [ $# -lt 1 ] && echo "Missing arguments" && echo "Usage: $0 daily|weekly" && exit 1
 [ $1 != "daily" ] && [ $1 != "weekly" ] && echo "Error, incorrect argument. Accepted values: daily, weekly. Usage: $0 daily|weekly" && exit 1
-[ $1 == "daily" ] && date=$(date -d "7 days ago" +%Y%m%d)
-[ $1 == "weekly" ] && date=$(date -d "53 weeks ago" +%Y%m%d)
+[ $1 == "daily" ] && date=$(date -d "$daily_backups days ago" +%Y%m%d)
+[ $1 == "weekly" ] && date=$(date -d "$weekly_backups weeks ago" +%Y%m%d)
 
 file_exists=$(s3cmd ls s3://pg_backup/$DBNAME-$date.gz)
 [ -z "$file_exists" ] && send_mail "Error pruning pg_backup bucket" "Something went wrong pruning pg_backup bucket. File s3://pg_backup/$DBNAME-$date.gz doesn't exists" && exit 1
